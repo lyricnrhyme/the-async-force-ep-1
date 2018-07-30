46fe8015c948a0c0042d3ba20abdd3678a4e431d
Jamie Yamashita <jamieyamashita@Jamies-MBP-2.hawaii.rr.com>
2018-07-29 23:29:19 -1000

    Found Han Solo

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-//   1. Get a Person object from the SWAPI, https://swapi.co/api/people/14/
-//       - fill in `person14Name` with the value of Person's `name`
-//       - fill in `person14Species` with the value of Person's (first) `species` (name)
+    // 1. Get a Person object from the SWAPI, https://swapi.co/api/people/4/
+//       - fill in `person4Name` with the value of Person's `name`
+//       - fill in `person4HomeWorld` with the value of Person's `homeworld` (name)
+    //   1. Get a Person object from the SWAPI, https://swapi.co/api/people/14/
+    //       - fill in `person14Name` with the value of Person's `name`
+
+
 //   1. Get a list of all the films from the SWAPI, https://swapi.co/api/films/
 //       - fill in `filmList` with a new `<li>` element for each `film`
 //           - fill in each film's `filmTitle` with the title of the `film`
@@ -11,9 +15,6 @@
 
 let starWars = new XMLHttpRequest();
 starWars.addEventListener("load", function(req) {
-    // 1. Get a Person object from the SWAPI, https://swapi.co/api/people/4/
-//       - fill in `person4Name` with the value of Person's `name`
-//       - fill in `person4HomeWorld` with the value of Person's `homeworld` (name)
     console.log("CHARACTERS: ", req.currentTarget.response)
 
     //PERSON 4
@@ -30,10 +31,29 @@ starWars.addEventListener("load", function(req) {
         })
         homeWorld.open("GET", homeWorldLink);
         homeWorld.send();
-
     })
     person4.open("GET", "https://swapi.co/api/people/4");
     person4.send();
+
+    //PERSON 14
+//       - fill in `person14Species` with the value of Person's (first) `species` (name)
+    person14 = new XMLHttpRequest();
+    person14.addEventListener("load", function(req14) {
+        //NAME
+        document.getElementById("person14Name").innerHTML = "Name: " + JSON.parse(req14.currentTarget.response).name;
+
+        //SPECIES
+        let homeWorldLink = JSON.parse(req4.currentTarget.response).homeworld;
+        homeWorld = new XMLHttpRequest();
+        homeWorld.addEventListener("load", function(req) {
+            document.getElementById("person4HomeWorld").innerHTML = "Home Planet: " + JSON.parse(req.currentTarget.response).name;
+        })
+        homeWorld.open("GET", homeWorldLink);
+        homeWorld.send();
+    })
+    person14.open("GET", "https://swapi.co/api/people/14");
+    person14.send();
+
 })
 
 starWars.open("GET", "https://swapi.co/api/people/");
