65a6222965bc5b8af05955e2f0ba297ffa2da434
Jamie Yamashita <jamieyamashita@Jamies-MBP-2.hawaii.rr.com>
2018-07-29 23:23:18 -1000

    Corrected link change, added home planet for person 4

--- app.js
+++ app.js
@@ -12,9 +12,28 @@
 let starWars = new XMLHttpRequest();
 starWars.addEventListener("load", function(req) {
     // 1. Get a Person object from the SWAPI, https://swapi.co/api/people/4/
+//       - fill in `person4Name` with the value of Person's `name`
 //       - fill in `person4HomeWorld` with the value of Person's `homeworld` (name)
-    console.log("test", req.currentTarget.response)
-    document.getElementById("person4Name").innerHTML = "Name: " + JSON.parse(req.currentTarget.response).name;
+    console.log("CHARACTERS: ", req.currentTarget.response)
+
+    //PERSON 4
+    person4 = new XMLHttpRequest();
+    person4.addEventListener("load", function(req4) {
+        //NAME
+        document.getElementById("person4Name").innerHTML = "Name: " + JSON.parse(req4.currentTarget.response).name;
+
+        //HOME WORLD
+        let homeWorldLink = JSON.parse(req4.currentTarget.response).homeworld;
+        homeWorld = new XMLHttpRequest();
+        homeWorld.addEventListener("load", function(req) {
+            document.getElementById("person4HomeWorld").innerHTML = "Home Planet: " + JSON.parse(req.currentTarget.response).name;
+        })
+        homeWorld.open("GET", homeWorldLink);
+        homeWorld.send();
+
+    })
+    person4.open("GET", "https://swapi.co/api/people/4");
+    person4.send();
 })
 
 starWars.open("GET", "https://swapi.co/api/people/");
